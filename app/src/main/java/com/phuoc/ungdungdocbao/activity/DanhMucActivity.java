fb1af5c15a43332453adfc17ffe97eaa5336ea6f
Nguyễn Đức Phước <nguyenducphuoc0802@gmail.com>
2023-10-31 16:38:58 +0700

    Initial commit

---
+++ app/src/main/java/com/phuoc/ungdungdocbao/activity/DanhMucActivity.java
@@ -0,0 +1,16 @@
+package com.phuoc.ungdungdocbao.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.phuoc.ungdungdocbao.R;
+
+public class DanhMucActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_muc);
+    }
+}
